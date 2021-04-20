beace3b197394ff5613d424906ef09b97792486c
vishalBindal <vishalbindal.06@gmail.com>
2021-04-21 04:03:08 +0530

    strategy 2 test 2

--- src/mainfile.c
+++ src/mainfile.c
@@ -102,9 +102,18 @@ void strat_2(double **A, double **L, double **U, int n) {
     // setting up openmp
     omp_set_num_threads(num_threads);
 
-    for (int i = 0; i < n; i++) {
-        U[i][i] = 1;
+    #pragma omp parallel
+    {
+        for (int i = 0; i < n; i++) {
+            #pragma omp sections nowait
+            {
+                #pragma omp section
+                U[i][i] = 1;
+            }
+        }
+        #pragma omp barrier
     }
+
     for (int j = 0; j < n; j++) {
         // calculate L[j][j] separately, since 2nd section depends on it
         // double sum = 0;
@@ -138,15 +147,22 @@ void strat_2(double **A, double **L, double **U, int n) {
             // #pragma omp section
             // {
                 for (int i = j; i < n; i++) {
-                    double sum = 0;
-                    for(int k = 0; k < j; k++) {
-                        sum = sum + L[j][k] * U[k][i];
+                    #pragma omp sections nowait
+                    {
+                        #pragma omp section
+                        {
+                            double sum = 0;
+                            for(int k = 0; k < j; k++) {
+                                sum = sum + L[j][k] * U[k][i];
+                            }
+                            if (L[j][j] == 0) {                
+                                exit(0);
+                            }
+                            U[j][i] = (A[j][i] - sum) / L[j][j];
+                        }
                     }
-                    if (L[j][j] == 0) {                
-                        exit(0);
-                    }
-                    U[j][i] = (A[j][i] - sum) / L[j][j];
                 }
+                #pragma omp barrier
             // }
         // }
         }
